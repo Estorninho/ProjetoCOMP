9416d997a030cc8b7e5c38c4296fd9e87dc30fb1
tuliosbhz <tulio.soares.bhz@gmail.com>
2021-01-18 15:07:42 +0000

    Supermagneto v0.1

--- src/main.cpp
+++ src/main.cpp
@@ -1,52 +1,26 @@
 #include <Arduino.h>
 #include <Wire.h>
 
-#define NUM_LEDS 8 // Number of LEDs per Stick
-#define DATA_PIN_X 2 // Pin 2 connected to RGB X
-#define DATA_PIN_Y 3 // Pin 3 connected to RGB Y
-
 /* The I2C address of the module */
-#define HMC5803L_Address 0x1E
+#define HMC5803L_Address 0x1E //Pointer to the registers addresses
 
 /* Register address for the X Y and Z data */
-#define X 3
-#define Y 7
-#define Z 5
-
-void setup() 
-{
-Serial.begin(9600); 
-Wire.begin();
-
-/* Initialise the module */ 
-Init_HMC5803L();
-}
-
-void loop() 
-{
-/* Read each sensor axis data and output to the serial port */
-Serial.print(HMC5803L_Read(X));
-Serial.print(" ");
-Serial.print(HMC5803L_Read(Y));
-Serial.print(" ");
-Serial.println(HMC5803L_Read(Z));
-/* Wait a little before reading again */
-delay(10);
-}
-
+#define X 3 //Data Output X MSB Register
+#define Y 7 //Data Output Y MSB Register
+#define Z 5 //Data Output Z MSB Register
 
 /* This function will initialise the module and only needs to be run once
 after the module is first powered up or reset */
 void Init_HMC5803L(void)
 {
 /* Set the module to 8x averaging and 15Hz measurement rate */
-Wire.beginTransmission(HMC5803L_Address);
-Wire.write(0x00);
-Wire.write(0x70);
+Wire.beginTransmission(HMC5803L_Address); //How the I2C start communication
+Wire.write(0x00); // A register address
+Wire.write(0x70); // In bits: 0 11 100 00(01)
 
 /* Set a gain of 5 */
-Wire.write(0x01);
-Wire.write(0xA0);
+Wire.write(0x01);// B register address
+Wire.write(0xA0);// In bits 101 00000
 Wire.endTransmission();
 }
 
@@ -75,6 +49,38 @@ Result = Wire.read() << 8;
 Result |= Wire.read();
 
 return Result;
+
+}
+
+
+void setup() 
+{
+Serial.begin(9600); 
+Wire.begin();
+
+/* Initialise the module */ 
+Init_HMC5803L();
+}
+
+void loop() 
+{
+/* Read each sensor axis data and output to the serial port */
+Serial.print(HMC5803L_Read(X));
+Serial.print(" ");
+Serial.print(HMC5803L_Read(Y));
+Serial.print(" ");
+Serial.println(HMC5803L_Read(Z));
+/* Wait a little before reading again */
+Serial.print(" ");
+int xvalue = HMC5803L_Read(X);
+int xinclination = map(xvalue, -280, 600, 0, 100); //Escala de 0 a 100%
+Serial.print(xinclination);
+Serial.print(" ");
+int yvalue = HMC5803L_Read(Y);
+int yinclination = map(yvalue, -330, 330, 0, 100); //Escala de 0 a 100%
+Serial.print(yinclination);
+
+delay(10);
 }
 
 /* End of Code */
